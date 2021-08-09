9c3b511eb387140329e7124ad622efa8cce0e11e
Uday Chugh <61309187+udaychugh@users.noreply.github.com>
2021-08-09 09:30:51 +0530

    Add files via upload

--- script/paid.js
+++ script/paid.js
@@ -3,21 +3,35 @@
 function getfeedback() {
     swal("Feedback Submitted", " ", "success");
     document.getElementById("feedback").style.visibility = "hidden";
+    var h2 = document.createElement("h2");
+    h2.innerHTML = "Thank You for your Feedback";
+    document.getElementById("mysection").appendChild(h2);
 }
 
 document.getElementById("great").onclick = function () {
     //document.getElementById("feed1").style.backgroundColor = '#45f545';
 
-    if (document.getElementById("feed2").style.backgroundColor != "#ffff3a") {
+    if (document.getElementById("feed2").style.backgroundColor != "#ffff3a" || document.getElementById("feed2").style.backgroundColor != "#fa2525") {
         document.getElementById("feed1").style.backgroundColor = '#45f545';
     }
 }
 
 document.getElementById("avg").onclick = function () {
-    document.getElementById("feed2").style.backgroundColor = '#ffff3a';
+    
+    if (document.getElementById("feed1").style.backgroundColor != "#45f545" || document.getElementById("feed3").style.backgroundColor != "#fa2525") {
+        document.getElementById("feed2").style.backgroundColor = '#ffff3a';
+    }
+    
+    
 }
 
 
 document.getElementById("disapp").onclick = function () {
-    document.getElementById("feed3").style.backgroundColor = '#fa2525';
+    
+    if (document.getElementById("feed1").style.backgroundColor != "#45f545" || document.getElementById("feed2").style.backgroundColor != "#ffff3a") {
+        document.getElementById("feed3").style.backgroundColor = '#fa2525';
+    }
+    
 }
+
+
